6dfcb81bd0e8b4da7c9c744a91d8a830e392e58a
tsunyoku <tsunyoku@gmail.com>
2021-06-23 21:43:09 +0100

    test pypi?

--- xevel/xevel.py
+++ xevel/xevel.py
@@ -1,4 +1,5 @@
 from typing import Coroutine, Dict, Union, Any, List
+from .codes import STATUS_CODES
 
 import socket
 import os
@@ -6,50 +7,6 @@ import signal
 import select
 import asyncio
 import re
-
-CODES = {
-    100: 'Continue',
-    101: 'Switching Protocols',
-    200: 'OK',
-    201: 'Created',
-    202: 'Accepted',
-    203: 'Non-Authoritative Information',
-    204: 'No Content',
-    205: 'Reset Content',
-    206: 'Partial Content',
-    300: 'Multiple Choices',
-    301: 'Moved Permanently',
-    302: 'Found',
-    303: 'See Other',
-    304: 'Not Modified',
-    305: 'Use Proxy',
-    306: '(Unused)',
-    307: 'Temporary Redirect',
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    402: 'Payment Required',
-    403: 'Forbidden',
-    404: 'Not Found',
-    405: 'Method Not Allowed',
-    406: 'Not Acceptable',
-    407: 'Proxy Authentication Required',
-    408: 'Request Timeout',
-    409: 'Conflict',
-    410: 'Gone',
-    411: 'Length Required',
-    412: 'Precondition Failed',
-    413: 'Request Entity Too Large',
-    414: 'Request-URI Too Long',
-    415: 'Unsupported Media Type',
-    416: 'Requested Range Not Satisfiable',
-    417: 'Expectation Failed',
-    500: 'Internal Server Error',
-    501: 'Not Implemented',
-    502: 'Bad Gateway',
-    503: 'Service Unavailable',
-    504: 'Gateway Timeout',
-    505: 'HTTP Version Not Supported'
-}
 
 class Endpoint:
     def __init__(self, path, method, handler):
@@ -127,7 +84,7 @@ class Request: # class to handle single request from client
         # TODO: handle multipart args/request
         
     async def send(self, code, b):
-        self.headers_list.insert(0, f'HTTP/1.1 {code} {CODES.get(code)}')
+        self.headers_list.insert(0, f'HTTP/1.1 {code} {STATUS_CODES.get(code)}')
         
         if b:
             self.headers_list.insert(1, f'Content-Length: {len(b)}')
